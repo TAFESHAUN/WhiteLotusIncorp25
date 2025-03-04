9dad39ac0c64b43376e8b98170f195bfe1d5b7ef
Shaun Gregory Stummer <141109489+TAFESHAUN@users.noreply.github.com>
2025-03-05 10:19:31 +1100

    Updated for week 5

--- WhiteLotusIncorp25/News.xaml.cs
+++ WhiteLotusIncorp25/News.xaml.cs
@@ -9,6 +9,16 @@ public partial class News : ContentPage
 
     private async void Button_Clicked(object sender, EventArgs e)
     {
+        //Button button = (Button)sender;
+        //Color color = Color.FromArgb("#FF0000");
+        //button.TextColor = color;
+        //newsBtn.TextColor = color;
+
+        //Grid return the exact cell I last clicked
+        //Canvas can have an arg of the exact X and Y coordinates of the click
+        //Slider value out of 100 or % of slide
+        //Entry -> current text in the entry
+
         NewsInfo passedNewsInfo = new NewsInfo
         {
             Title = CoolNews.Text,
@@ -20,7 +30,7 @@ public partial class News : ContentPage
         //passedNewsInfo2.Title = "Cool News 2";
         //passedNewsInfo2.Description = "Some News Description 2";
         //passedNewsInfo2.Image = "news2.jpg";
-
+        DisplayAlert("News", passedNewsInfo.Title, "OK");
 
         await Navigation.PushAsync(new NewsDetails(passedNewsInfo));
     }
